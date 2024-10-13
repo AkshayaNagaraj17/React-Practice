9674edaccd914e91457b4223bb8e79ccca511360
AkshayaNagaraj <cnagaraj332@gmail.com>
2024-10-13 14:02:36 +0530

    Card practiced

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import Header from "./Header.jsx"
-import Footer from "./Footer.jsx"
-import Food from "./Food.jsx"
+import Card from "./Card.jsx"
 function App()
 {
-  return(
-    <>
-    <Header>
-    </Header>
-    <Footer></Footer>
-    <Food></Food>
-    </>
-  )
+  return(<>
+  <Card></Card>
+    <Card>
+    </Card>
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+    <Card></Card>
+  </>
+    
+   )
 }
- 
 export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+function Card()
+{
+    return(
+        <div className="card">
+            <img src="https://via.placeholder.com/150" alt="Cat pic">
+            </img>
+            <h2>Aksh</h2>
+            <p>Im doing react practice session now</p>
+        </div>
+    )
+}
+export default Card
